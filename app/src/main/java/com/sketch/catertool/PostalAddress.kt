e88e7e6c50e87946bc2c01231e952b083ba0839d
salhotra24 <salhotra95@gmail.com>
2022-11-30 14:22:29 +0530

    Change 1-7 screen

---
+++ app/src/main/java/com/sketch/catertool/PostalAddress.kt
@@ -0,0 +1,11 @@
+package com.sketch.catertool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostalAddress : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postal_address)
+    }
+}
