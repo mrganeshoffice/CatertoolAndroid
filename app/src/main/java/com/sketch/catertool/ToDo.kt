37ee049246951a7de110c4b8fb3d9ca53fad4255
salhotra24 <salhotra95@gmail.com>
2022-11-30 16:44:28 +0530

    todo

---
+++ app/src/main/java/com/sketch/catertool/ToDo.kt
@@ -0,0 +1,11 @@
+package com.sketch.catertool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToDo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do)
+    }
+}
